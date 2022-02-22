857b7f57a088bc225e926bf2efae5a5dd9edc230
bxhi <i.mesbahi@esi-sba.dz>
2022-02-22 14:51:07 +0100

    version0.0

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("hello world");
+const myName='Haitham';
+var myAge=18;
+const isDone=true;
+console.log(myName,myAge,isDone);
+let x=15;
+let y="20";
+let i=1
+console.log(x**y);
+console.log(y/x);
+console.log(y%x);
+console.log(x!=y);
+console.log(x==y);
+let bill=400;
+bill>50 && bill<300 ? console.log(bill*0.15):console.log(bill*0.20);
+let x=
+let y=
+let z=
+const calcAverage(x,y,z)=>(x+y+z)/3;
+avgDolphins=calcAverage(x,y,z);
+avgkoalas=calcAverage(x1,y1,z1);
+function checkwinner(avgDolphins,avgKoalas){
+    if(avgDolphins>=2*avgKoalas){console.log('Dolphins win  ' +(avgDolphins,)}
+}
+
+
+
+
+
+
+
+
